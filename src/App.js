77e332ffc362a821638abef4cce8b7cdda8cebed
Aprendiz TIC <Aprendiz TIC@TIC011.dime.net.co>
2022-08-12 13:21:45 -0500

    portafolio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {NavBar} from './components/NavBar';
-import {Banner} from './components/Banner';
+/* import {Banner} from './components/Banner'; */
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
     <NavBar/>
-    <Banner/>
+   {/*  <Banner/> */}
     </div>
   );
 }
