613c57e6af1a452c0314bc015d23a40fe394ee3a
Aprendiz TIC <Aprendiz TIC@TIC011.dime.net.co>
2022-09-19 15:07:46 -0500

    portafolio

---
+++ src/components/Projects.js
@@ -0,0 +1,104 @@
+import { Col,Container,Tab,Row,Nav} from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+
+
+export const Projects = () => {
+   const projects = [
+   
+        {
+            title: "Bussiness Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Bussiness Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Bussiness Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+        },
+        {
+            title: "Bussiness Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Bussiness Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Bussiness Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+        },
+    
+   ];
+
+   return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                    <h2>Projects</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident, corporis consequatur eum consectetur pariatur asperiores, aliquid et modi saepe voluptate ducimus, ipsa ab alias repudiandae odio optio hic unde id</p>
+                   
+
+      <Tab.Container id="projects-tabs" defaultActiveKey="first">
+       <Nav variant="pills"  className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+      <Nav.Item>
+        <Nav.Link eventKey="first">Tab One</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="second">Tab Two</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="third" >
+          Tab Three
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+
+    <Tab.Content>
+      <Tab.Pane eventKey="first">
+        <Row>
+            {
+                projects.map((project, index)=>{
+                    return (
+                        <ProjectCard key={index}{...project}/>
+                    )
+                })
+            }
+        </Row>
+      </Tab.Pane>
+      <Tab.Pane eventKey="second">
+         Lorem Ipsum
+      </Tab.Pane>
+
+     <Tab.Pane eventKey="third">
+       Lorem Ipsum
+     </Tab.Pane>
+
+    </Tab.Content>
+  
+  </Tab.Container>
+  
+
+
+
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-right" src={colorSharp2}></img>
+
+        </section>
+    ) 
+
+}
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,10 @@
 import { Container,Col,Row } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills =() => {
     const responsive = {
@@ -22,7 +26,41 @@ export const Skills =() => {
         }
     };
 
-    return{
+    return(
+        <section className="skill" id="skills">
+            <Container>
+                <Row>
+                    <Col>
+                    <div className="skill-bx">
+                        <h2>
+                            Skills
+                        </h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti amet modi ducimus delectus molestiae illum soluta velit, ipsum, omnis saepe dignissimos autem laborum nobis eligendi quis laudantium aliquam praesentium vitae.</p>
+                        <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                            <div className="item">
+                                <img src={meter1} alt="Image"/>
+                                <h5>Web Development</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter2} alt="Image"/>
+                                <h5>Brand Identity</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter3} alt="Image"/>
+                                <h5>Logo Design</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter1} alt="Image"/>
+                                <h5>Web Development</h5>
+                            </div>
+                        </Carousel>
+                    </div>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-left" src={colorSharp} />
+        </section>
+    )
         
-    }
+    
 }
